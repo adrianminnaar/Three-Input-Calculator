a887f1f248e6b5f0a6ec65198b32df51aab9e397
adrianminnaar <adrianminaar29@gmail.com>
2023-01-25 20:32:35 +0200

    fix

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 
+
+
+
+
+function add(params) {
+    var num = document.getElementById('input1').value;
+    var num1 = document.getElementById('input2').value;
+    document.getElementById('equal').value= num + num1
+    console.log(num + num1)
+
+}
